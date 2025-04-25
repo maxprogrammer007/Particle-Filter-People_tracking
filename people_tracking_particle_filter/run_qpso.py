da1b2a49d9334d3e0ac734158f72549ce7bec6b4
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-04-25 22:30:53 +0530

    Update run_qpso.py

--- people_tracking_particle_filter/run_qpso.py
+++ people_tracking_particle_filter/run_qpso.py
@@ -1,4 +1,4 @@
 from qpso_optimizer import run_qpso
 
 if __name__ == "__main__":
-    best_config, best_fit = run_qpso(pop_size=4, max_gen=2)  # Adjust for final runs
+    run_qpso(pop_size=4, max_gen=2)  # for fast testing
