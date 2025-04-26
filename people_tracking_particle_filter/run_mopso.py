c5e2d2ef477ae691b5b913be46b3021aea4d285d
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-04-26 16:46:33 +0530

    Update run_mopso.py

--- people_tracking_particle_filter/run_mopso.py
+++ people_tracking_particle_filter/run_mopso.py
@@ -1,3 +1,8 @@
 from mopso_optimizer import run_mopso
+ # Use small values to test first
 
-run_mopso(pop_size=4, generations=2)  # Use small values to test first
+from mopso_plot_pareto import plot_mopso_pareto
+
+# after run_mopso
+best, best_fit = run_mopso(pop_size=4, generations=2)
+plot_mopso_pareto(archive)  # archive = final archive list you have
