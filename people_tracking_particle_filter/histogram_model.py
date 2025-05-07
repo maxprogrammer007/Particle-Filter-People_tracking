59c5cf5c8fa2c3be1150bee13d864eebbe6d810c
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-05-07 12:29:17 +0530

    Delete histogram_model.py

--- people_tracking_particle_filter/histogram_model.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def get_color_histogram(image):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    hist = cv2.calcHist([hsv], [0, 1], None, [32, 32], [0, 180, 0, 256])
-    cv2.normalize(hist, hist)
-    return hist
